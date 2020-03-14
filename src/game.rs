39d54de418b9a189ffd26fc28831756ff332e597
edubrunaldi <eduardobrunaldi@protonmail.com>
2020-03-14 13:30:23 -0300

    game working

--- src/game.rs
+++ src/game.rs
@@ -6,7 +6,8 @@ pub struct Game {
     player0: i8, // count n plays of player0, player0 always start
     player1: i8,
     finished: bool,
-    board: Board
+    board: Board,
+    input_regex: String
 }
 
 impl Game {
@@ -15,7 +16,8 @@ impl Game {
         player0: 0,
         player1: 0,
         finished: false,
-        board: Board::new()
+        board: Board::new(),
+        input_regex: r"^[1-3] [1-3]$".to_string()
     }
   }
 
@@ -26,27 +28,69 @@ impl Game {
         break;
       }
     }
+    self.board.print_board();
+    if self.board.someone_win() {
+      let player_won: i8 = if self.player0 > self.player1 {0} else {1};
+
+      println!("\t\n Congratulation Player{}, you won {}!!", player_won, '\u{1f3c6}');
+    } else {
+      println!("\t\n Oh! You guys draw =/");
+    }
   }
 }
 
 impl Game {
   fn player_turn(&mut self) {
-    let player_turn = if self.player0 >= self.player1 {&self.player0} else {&self.player1};
+    let player_turn: i8 = if self.player0 <= self.player1 {0} else {1};
     println!("\n\t It is your turn Player{}\n", player_turn);
     self.board.print_board();
     let mut result = utils::get_input("Insert position X Y (e.g > 1 2)".to_string());
     result = result.trim().to_string();
-    while !utils::validate_regex("1 2", &result) {
-      result = utils::get_input(format!("({}) is invalid position, try (> row col, e.g. > 1 2)", result));
+    let values: &Vec<&str> = &result.split_ascii_whitespace().collect();
+    let mut row: usize = values[0].parse::<usize>().unwrap();
+    let mut col: usize = values[1].parse::<usize>().unwrap();
+    loop {
+      let input_msg: String;
+      if !utils::validate_regex(&self.input_regex, &result) {
+        input_msg = format!("({}) is invalid position, try (> row col, e.g. > 1 2)", result);
+      } else if !self.board.is_valid_space(&row, &col) {
+        input_msg = format!("({}) already used, try another position", result);
+      } else {
+        break;
+      }
+      result = utils::get_input(input_msg);
       result = result.trim().to_string();
+      let values: &Vec<&str> = &result.split_ascii_whitespace().collect();
+      let (update_row, update_col) = self.get_position(values);
+      row = update_row;
+      col = update_col;
     }
-    self.do_action(result);
+
+    self.do_action(&row, &col, player_turn);
+
+    // update played turn
+    if self.player0 <= self.player1 {
+      self.player0 += 1;
+    } else {
+      self.player1 += 1;
+    }
+
+    // update finished variable
+    self.finished = self.is_finished();
   }
 
-  fn do_action(&mut self, action: String) {
-    let position: Vec<&str> = action.split_ascii_whitespace().collect();
-    let row: &usize = &position[0].parse::<usize>().unwrap();
-    let col: &usize = &position[1].parse::<usize>().unwrap();
-    self.board.draw(*row, *col, Draw::Circle);
+  fn get_position(&self, values: &Vec<&str>) -> (usize, usize) {
+    let row: usize = values.clone()[0].parse::<usize>().unwrap();
+    let col: usize = values[1].parse::<usize>().unwrap();
+    (row, col)
+  }
+
+  fn do_action(&mut self, row: &usize, col: &usize, player_turn: i8) {
+    let item: Draw = if player_turn == 0 {Draw::Circle} else {Draw::CrossMark};
+    self.board.draw(row, col, item);
+  }
+
+  fn is_finished(&self) -> bool {
+    self.player0 + self.player1 >= 9 || self.board.someone_win()
   }
 }
--- src/game/board.rs
+++ src/game/board.rs
@@ -13,8 +13,8 @@ pub enum Draw {
 impl Board {
   pub fn new() -> Self {
     Board {
-      circle: '\u{26db}',
-      cross_mark: '\u{274c}',
+      circle: '\u{2b55}',
+      cross_mark: '\u{2694}',
       spaces: vec!(' '; 9),
     }
   }
@@ -27,10 +27,33 @@ impl Board {
     println!("\t\t3   {} | {} | {} \n", self.spaces[6], self.spaces[7], self.spaces[8]);
   }
 
-  pub fn draw(&mut self, row: usize, col: usize, item: Draw) {
+  pub fn draw(&mut self, row: &usize, col: &usize, item: Draw) {
     match item {
       Draw::Circle => self.spaces[(row-1)*3 + (col-1) ] = self.circle,
       Draw::CrossMark => self.spaces[(row-1)*3 + (col-1) ] = self.cross_mark
     };
+  }
+
+  pub fn is_valid_space(&self, row: &usize, col: &usize) -> bool {
+    self.spaces[(row-1)*3 + (col-1)] == ' '
+  }
+
+  // There is only 8 ways to win
+  // 3 rows
+  // 3 cols
+  // 2 diagonal
+  pub fn someone_win(&self) -> bool {
+    // rows
+    (self.spaces[0] != ' ' && self.spaces[0] == self.spaces[1] && self.spaces[1] == self.spaces[2] && self.spaces[0] == self.spaces[2]) ||
+    (self.spaces[3] != ' ' && self.spaces[3] == self.spaces[4] && self.spaces[4] == self.spaces[5] && self.spaces[3] == self.spaces[5]) ||
+    (self.spaces[6] != ' ' && self.spaces[6] == self.spaces[7] && self.spaces[7] == self.spaces[8] && self.spaces[6] == self.spaces[8]) ||
+    // cols
+    (self.spaces[0] != ' ' && self.spaces[0] == self.spaces[3] && self.spaces[3] == self.spaces[6] && self.spaces[0] == self.spaces[6]) ||
+    (self.spaces[1] != ' ' && self.spaces[1] == self.spaces[4] && self.spaces[4] == self.spaces[7] && self.spaces[1] == self.spaces[7]) ||
+    (self.spaces[2] != ' ' && self.spaces[2] == self.spaces[5] && self.spaces[5] == self.spaces[8] && self.spaces[2] == self.spaces[8]) ||
+    // left to right diagonal
+    (self.spaces[0] != ' ' && self.spaces[0] == self.spaces[4] && self.spaces[4] == self.spaces[8] && self.spaces[0] == self.spaces[8]) ||
+    // right to left diagonal
+    (self.spaces[2] != ' ' && self.spaces[2] == self.spaces[4] && self.spaces[4] == self.spaces[6] && self.spaces[2] == self.spaces[6]) 
   }
 }
